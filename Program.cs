1832391a275444c8fa4e6a751e7176f02f7f27ca
Kristof Maar <maarkristof@gmail.com>
2020-05-15 10:36:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HalalBeadando_ISGK7K
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
